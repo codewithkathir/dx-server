08641f3a9eeb78b1d4bddb926144c7a452163ebc
Your Name <you@example.com>
2026-09-07 01:23:03 +0530

    app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -25,8 +25,14 @@ export function createApp(options: CreateAppOptions = {}): express.Application {
   app.use(helmet());
   app.use(
     cors({
-      origin: ["http://localhost:3000", "http://localhost:5173, *"],// config.cors.origin,
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://app.dxrecord.com",
+      ],
       credentials: true,
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
     })
   );
   app.use(compression());
